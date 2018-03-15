9edd9b5d3b3139b37a75f12e14f09940ec529620
ARKALYK AKASH <arkalyk.akash@nu.edu.kz>
2018-03-15 15:51:18 +0100

    checkIfBusy method added.

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const RNCallKitDidDisplayIncomingCall = 'RNCallKitDidDisplayIncomingCall';
 const RNCallKitDidPerformSetMutedCallAction = 'RNCallKitDidPerformSetMutedCallAction';
 
 export default class RNCallKit {
+    static checkIfBusy = _RNCallKit.checkIfBusy;
+
     static addEventListener(type, handler) {
         if (Platform.OS !== 'ios') return;
         var listener;
