abcf350db805807ed8fd7f04bfd3a7bcf83297af
Ayoub Laaziz <a.laaziz@piman2-0.fr>
2018-12-13 11:07:23 +0100

    add reject

--- ios/RNCallKit/RNCallKit.m
+++ ios/RNCallKit/RNCallKit.m
@@ -211,6 +211,7 @@ RCT_EXPORT_METHOD(checkIfInCall:(NSString *)uuidString
     }
     @catch (NSException *exception) {
         NSLog(@"[RNCallKit][checkIfInCall] exception %@", exception.reason);
+        reject(@{@"error": exception.reason});
     } 
 }
 
